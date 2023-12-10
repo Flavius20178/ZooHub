75baa941f0bb3a3806d7b456dc1cc91f36d68d4a
Flavius20178 <flaviusastratinei03@gmail.com>
2023-12-10 23:48:48 +0200

    Update content

--- pages/events.php
+++
@@ -1,6 +0,0 @@
-<?php
-$eventsTable = new Table('events');
-$events = $eventsTable->findInDatabase('event_archived', 'false');
-
-$title = "Zoo - Events";
-$content = loadTemplate('../templates/events_template.php', ['events' => $events]);
